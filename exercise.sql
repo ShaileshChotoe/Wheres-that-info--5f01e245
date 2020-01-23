6ae82bee3da636018ecf7ac9a18c7d3cbea6b348
unknown <2120367@talnet.nl>
2020-01-23 13:31:39 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,10 @@
+use netland;
+
+--select title from series where has_won_awards = 1;
+--select title from series where rating > 2.5;
+--select title from series where country = 'NL' AND language = 'NL';
+--select title from series where seasons < 5;
+--select title from series order by rating desc;
+--select title from series where seasons < 3 || seasons > 20;
+--select title from series where title like '%Th%';
+--select title from series where seasons != 3;
